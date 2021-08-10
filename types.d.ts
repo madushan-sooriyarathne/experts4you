19483da723191408693a48d58beb2bfd8cbb3fe4
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2021-08-10 15:31:52 +0530

    added some new types

--- types.d.ts
+++ types.d.ts
@@ -1,5 +1,31 @@
 declare global {
   type AlignmentType = "center" | "left" | "right" | "justify";
+
+  interface Image {
+    src: string;
+    alt?: string;
+    blurUrl: string;
+  }
+
+  interface Service {
+    name: string;
+    id: string;
+    description: string;
+    image: Image;
+  }
+
+  interface Stats {
+    number: string;
+    label: string;
+  }
+
+  interface HeroData {
+    title: string;
+    subText: string;
+    route: string;
+    stats: Stats[];
+    image: Image;
+  }
 }
 
 export {};
