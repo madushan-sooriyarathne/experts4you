eaf539fd35fdf6cc3815e73e39fff8fbe1daeba4
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2021-08-10 15:34:15 +0530

    added hero & heroSection components to index.ts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,41 @@
-const HomePage: React.FC = (): JSX.Element => {
-  return <h1>This is homepage</h1>;
+import {
+  GetStaticProps,
+  GetStaticPropsContext,
+  GetStaticPropsResult,
+} from "next";
+import Page from "@components/layout/common/page";
+import Hero from "@components/layout/homepage/hero";
+import HeroSection from "@components/layout/homepage/hero-section";
+
+import { services, heroData } from "@site-data";
+
+interface Props {
+  services: Service[];
+  heroData: HeroData;
+}
+
+const HomePage: React.FC<Props> = ({
+  services,
+  heroData,
+}: Props): JSX.Element => {
+  return (
+    <Page>
+      <Hero services={services} />
+      <HeroSection heroData={heroData} />
+    </Page>
+  );
 };
 
+const getStaticProps: GetStaticProps = async (
+  context: GetStaticPropsContext
+): Promise<GetStaticPropsResult<Props>> => {
+  return {
+    props: {
+      services,
+      heroData,
+    },
+  };
+};
+
+export { getStaticProps };
 export default HomePage;
