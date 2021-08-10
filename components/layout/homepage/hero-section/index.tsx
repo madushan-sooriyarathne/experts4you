d384949182d27c1d77d96e4cb1f760287eb29b2d
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2021-08-10 15:30:53 +0530

    created HeroSection component

---
+++ components/layout/homepage/hero-section/index.tsx
@@ -0,0 +1,52 @@
+import PrimaryButton from "@components/buttons/primary-button";
+import PrimaryHeading from "@components/headings/primary-heading";
+import ImageComponent from "@components/image-component";
+import Paragraph from "@components/paragraph";
+import {
+  Container,
+  ImageContainer,
+  TextContainer,
+  StatGroup,
+  StatItem,
+  StatNumber,
+  StatLabel,
+  ContainerBG,
+  ContainerOverlay,
+} from "./styles";
+
+interface Props {
+  heroData: HeroData;
+}
+
+const HeroSection: React.FC<Props> = ({ heroData }: Props): JSX.Element => {
+  return (
+    <Container>
+      <ContainerBG />
+      <ContainerOverlay />
+      <TextContainer>
+        <PrimaryHeading alignment="left">{heroData.title}</PrimaryHeading>
+        <Paragraph alignment="left">{heroData.subText}</Paragraph>
+        <PrimaryButton type="route" route={heroData.route}>
+          Learn More
+        </PrimaryButton>
+        <StatGroup>
+          {heroData.stats.map((stat, index) => (
+            <StatItem id={`stat-${index}`}>
+              <StatNumber>{stat.number}</StatNumber>
+              <StatLabel>{stat.label}</StatLabel>
+            </StatItem>
+          ))}
+        </StatGroup>
+      </TextContainer>
+      <ImageContainer>
+        <ImageComponent
+          image={heroData.image}
+          objectFit="contain"
+          pos={{ x: "center", y: "bottom" }}
+        />
+      </ImageContainer>
+    </Container>
+  );
+};
+
+export default HeroSection;
---
+++ components/layout/homepage/hero-section/styles.ts
@@ -0,0 +1,111 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  grid-column: full-start / full-end;
+
+  width: 100%;
+  height: 100vh;
+  padding: 15rem 10rem 0 10rem;
+  margin-top: -15rem;
+
+  position: relative;
+  display: grid;
+  grid-template-columns: repeat(2, minmax(min-content, 1fr));
+  grid-template-rows: 1fr;
+  grid-template-areas: "co im";
+  column-gap: 5rem;
+
+  clip-path: polygon(0 0, 100% 0, 100% 100%, 0 85%);
+`;
+
+const ContainerBG = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  z-index: -2;
+
+  background-image: ${(props) => `url("/assets/svg/bg.svg"),
+    ${props.theme.gradients.mainBg}`};
+  background-position: center center;
+  background-repeat: no-repeat;
+  background-size: cover;
+`;
+
+const ContainerOverlay = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  z-index: -1;
+  backdrop-filter: blur(100px);
+`;
+
+const TextContainer = styled.div`
+  grid-area: co;
+  padding: 10rem;
+
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+  justify-content: center;
+  row-gap: 4rem;
+`;
+
+const ImageContainer = styled.div`
+  grid-area: im;
+  width: 100%;
+  height: 100%;
+`;
+
+const StatGroup = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, min-content);
+  align-items: start;
+  justify-items: start;
+  column-gap: 5rem;
+`;
+
+const StatItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  row-gap: 1rem;
+`;
+
+const StatNumber = styled.h2`
+  font-size: 4rem;
+  font-weight: 900;
+  font-family: ${(props) => props.theme.fonts.primary};
+  letter-spacing: 1px;
+  color: ${(props) => props.theme.colors.blackMedium};
+`;
+
+const StatLabel = styled.p`
+  font-size: 1.5rem;
+  font-weight: 400;
+  font-family: ${(props) => props.theme.fonts.primary};
+  letter-spacing: 0.5px;
+  text-transform: uppercase;
+  text-align: left;
+  color: ${(props) => props.theme.colors.blackMedium};
+`;
+
+export {
+  Container,
+  ContainerBG,
+  ContainerOverlay,
+  TextContainer,
+  ImageContainer,
+  StatGroup,
+  StatItem,
+  StatNumber,
+  StatLabel,
+};
