3f31d4c7a5788d95cbcd5e59a0428f1d6f98d2df
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2021-08-11 11:15:29 +0530

    added main services section

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,9 @@ import Page from "@components/layout/common/page";
 import Hero from "@components/layout/homepage/hero";
 import HeroSection from "@components/layout/homepage/hero-section";
 
+import Card from "@components/card";
+import GridLayout from "@components/layout/common/grid-layout";
+
 import { services, heroData } from "@site-data";
 
 interface Props {
@@ -22,6 +25,17 @@ const HomePage: React.FC<Props> = ({
     <Page>
       <Hero services={services} />
       <HeroSection heroData={heroData} />
+      <GridLayout gap="8rem">
+        {services.map((service) => (
+          <Card
+            key={`service-${service.id}`}
+            title={service.name}
+            description={service.description}
+            route={`/services/${service.id}`}
+            icon={service.icon}
+          />
+        ))}
+      </GridLayout>
     </Page>
   );
 };
