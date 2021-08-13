433c924dcc856ee26a4bba88c28a2961f9ec0dcc
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2021-08-14 00:06:48 +0530

    Created SubHeading component

---
+++ components/headings/sub-heading/styles.ts
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+interface HeadingProps {
+  alignment: AlignmentType;
+  light: boolean;
+}
+
+const Heading = styled.h4<HeadingProps>`
+  font-size: 1.6rem;
+  font-weight: 400;
+  font-family: ${(props) => props.theme.fonts.primary};
+  letter-spacing: 0.5px;
+  text-align: ${(props) => props.alignment};
+  color: ${(props) =>
+    props.light ? props.theme.colors.white : props.theme.colors.black};
+`;
+
+export { Heading };
