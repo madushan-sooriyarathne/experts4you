d458593dbf9ffeb056ba635278721dfb3cbaf786
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2021-08-14 22:42:28 +0530

    created WhoAreWe component

---
+++ components/layout/homepage/who-are-we/index.tsx
@@ -0,0 +1,66 @@
+import PrimaryButton from "@components/buttons/primary-button";
+import PrimaryHeading from "@components/headings/primary-heading";
+import SecondaryHeading from "@components/headings/secondary-heading";
+import SubHeading from "@components/headings/sub-heading";
+import GridLayout from "@components/layout/common/grid-layout";
+import Paragraph from "@components/paragraph";
+import { BoxItem, Description, WhoWeAreContainer } from "./styles";
+
+const WhoWeAre: React.FC = (): JSX.Element => {
+  return (
+    <WhoWeAreContainer>
+      <PrimaryHeading>Who We Are</PrimaryHeading>
+      <Description>
+        <SubHeading>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum
+          dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+          incididunt ut labore et dolore magna aliqua.
+        </SubHeading>
+      </Description>
+      <GridLayout>
+        <BoxItem>
+          <svg>
+            <use xlinkHref="/assets/svg/sprites.svg#vision" />
+          </svg>
+          <SecondaryHeading>Vision</SecondaryHeading>
+          <Paragraph alignment="left">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem
+            ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua.
+          </Paragraph>
+        </BoxItem>
+        <BoxItem>
+          <svg>
+            <use xlinkHref="/assets/svg/sprites.svg#mission" />
+          </svg>
+          <SecondaryHeading>Mission</SecondaryHeading>
+          <Paragraph alignment="left">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem
+            ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua.
+          </Paragraph>
+        </BoxItem>
+        <BoxItem>
+          <svg>
+            <use xlinkHref="/assets/svg/sprites.svg#goal" />
+          </svg>
+          <SecondaryHeading>Goal</SecondaryHeading>
+          <Paragraph alignment="left">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem
+            ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua.
+          </Paragraph>
+        </BoxItem>
+      </GridLayout>
+      <PrimaryButton type="route" route="/about">
+        About us
+      </PrimaryButton>
+    </WhoWeAreContainer>
+  );
+};
+
+export default WhoWeAre;
---
+++ components/layout/homepage/who-are-we/styles.ts
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+
+const WhoWeAreContainer = styled.section`
+  grid-column: content-start / content-end;
+
+  width: 100%;
+  height: auto;
+
+  display: flex;
+  flex-direction: column;
+  row-gap: 5rem;
+  align-items: center;
+  justify-content: flex-start;
+`;
+
+const Description = styled.div`
+  width: 60%;
+`;
+
+const BoxItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-start;
+  row-gap: 3rem;
+
+  padding: 3rem;
+
+  & > svg {
+    width: 5rem;
+    height: 5rem;
+  }
+`;
+
+export { WhoWeAreContainer, Description, BoxItem };
