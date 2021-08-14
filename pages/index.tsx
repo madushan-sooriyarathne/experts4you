b2c5bc1fbb4e4d932f5a0c7abc7566c88c16dbaf
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2021-08-14 22:43:32 +0530

    added homepage components

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,13 +4,14 @@ import {
   GetStaticPropsResult,
 } from "next";
 import Page from "@components/layout/common/page";
-import Hero from "@components/layout/homepage/hero";
 import HeroSection from "@components/layout/homepage/hero-section";
 
 import Card from "@components/card";
 import GridLayout from "@components/layout/common/grid-layout";
 
 import { services, heroData } from "@site-data";
+import TitledGrid from "@components/layout/common/titled-grid";
+import WhoWeAre from "@components/layout/homepage/who-are-we";
 
 interface Props {
   services: Service[];
@@ -23,7 +24,7 @@ const HomePage: React.FC<Props> = ({
 }: Props): JSX.Element => {
   return (
     <Page>
-      <Hero services={services} />
+      {/* <Hero services={services} /> */}
       <HeroSection heroData={heroData} />
       <GridLayout gap="8rem">
         {services.map((service) => (
@@ -36,6 +37,23 @@ const HomePage: React.FC<Props> = ({
           />
         ))}
       </GridLayout>
+      <WhoWeAre />
+      <TitledGrid
+        heading="What we do"
+        subHeading="Services that lead the way to better business"
+        route="/services"
+      >
+        {services.map((service) => (
+          <Card
+            withButton
+            key={`service-${service.id}`}
+            title={service.name}
+            description={service.description}
+            route={`/services/${service.id}`}
+            icon={service.icon}
+          />
+        ))}
+      </TitledGrid>
     </Page>
   );
 };
