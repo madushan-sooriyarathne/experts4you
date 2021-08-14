8ea3b073f6b3665295470ee5c0d91ac64469d20a
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2021-08-15 01:42:48 +0530

    added testimonials section to homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,24 +3,27 @@ import {
   GetStaticPropsContext,
   GetStaticPropsResult,
 } from "next";
+
 import Page from "@components/layout/common/page";
 import HeroSection from "@components/layout/homepage/hero-section";
-
 import Card from "@components/card";
 import GridLayout from "@components/layout/common/grid-layout";
-
-import { services, heroData } from "@site-data";
 import TitledGrid from "@components/layout/common/titled-grid";
 import WhoWeAre from "@components/layout/homepage/who-are-we";
+import TestimonialsSection from "@components/layout/common/testimonials";
+
+import { services, heroData, testimonials } from "@site-data";
 
 interface Props {
   services: Service[];
   heroData: HeroData;
+  testimonials: Testimonial[];
 }
 
 const HomePage: React.FC<Props> = ({
   services,
   heroData,
+  testimonials,
 }: Props): JSX.Element => {
   return (
     <Page>
@@ -54,6 +57,7 @@ const HomePage: React.FC<Props> = ({
           />
         ))}
       </TitledGrid>
+      <TestimonialsSection testimonials={testimonials} />
     </Page>
   );
 };
@@ -65,6 +69,7 @@ const getStaticProps: GetStaticProps = async (
     props: {
       services,
       heroData,
+      testimonials,
     },
   };
 };
