8db777a3a4713a9b29c86c89d0d3e7684949e512
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2021-08-17 22:09:34 +0530

    added homepage components

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,46 +5,49 @@ import {
 } from "next";
 
 import Page from "@components/layout/common/page";
+import MainServicesSection from "@components/layout/homepage/main-services";
 import HeroSection from "@components/layout/homepage/hero-section";
 import Card from "@components/card";
-import GridLayout from "@components/layout/common/grid-layout";
 import TitledGrid from "@components/layout/common/titled-grid";
 import WhoWeAre from "@components/layout/homepage/who-are-we";
 import TestimonialsSection from "@components/layout/common/testimonials";
 
-import { services, heroData, testimonials } from "@site-data";
+import {
+  services,
+  heroData,
+  testimonials,
+  partners,
+  ourValues,
+} from "@site-data";
+import React from "react";
+import StaticCard from "@components/static-card";
 
 interface Props {
   services: Service[];
   heroData: HeroData;
   testimonials: Testimonial[];
+  ourValues: CardContentType[];
+  partners: Partner[];
 }
 
 const HomePage: React.FC<Props> = ({
   services,
   heroData,
   testimonials,
+  ourValues,
+  partners,
 }: Props): JSX.Element => {
   return (
-    <Page>
-      {/* <Hero services={services} /> */}
+    <Page title="Experts 4 You - Your trusted partner in business">
       <HeroSection heroData={heroData} />
-      <GridLayout gap="8rem">
-        {services.map((service) => (
-          <Card
-            key={`service-${service.id}`}
-            title={service.name}
-            description={service.description}
-            route={`/services/${service.id}`}
-            icon={service.icon}
-          />
-        ))}
-      </GridLayout>
+      <MainServicesSection services={services} />
+
       <WhoWeAre />
       <TitledGrid
         heading="What we do"
         subHeading="Services that lead the way to better business"
         route="/services"
+        id="what-we-do"
       >
         {services.map((service) => (
           <Card
@@ -57,7 +60,16 @@ const HomePage: React.FC<Props> = ({
           />
         ))}
       </TitledGrid>
-      <TestimonialsSection testimonials={testimonials} />
+      <TitledGrid
+        heading="Why us"
+        subHeading="Our job is not just to complete a project, we are here for you to help you shape and realise your vision."
+        id="why-us"
+      >
+        {ourValues.map((value, index) => (
+          <StaticCard content={value} key={`static-card-${index}`} />
+        ))}
+      </TitledGrid>
+      <TestimonialsSection testimonials={testimonials} partners={partners} />
     </Page>
   );
 };
@@ -70,6 +82,8 @@ const getStaticProps: GetStaticProps = async (
       services,
       heroData,
       testimonials,
+      ourValues,
+      partners,
     },
   };
 };
