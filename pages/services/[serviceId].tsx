e0698bc570233c4ef3fa5a46d36cd9523394c262
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2021-08-22 17:26:25 +0530

    created [serviceId].ts rotue for individual service

---
+++ pages/services/[serviceId].tsx
@@ -0,0 +1,72 @@
+import {
+  GetStaticPaths,
+  GetStaticPathsContext,
+  GetStaticPathsResult,
+  GetStaticProps,
+  GetStaticPropsContext,
+  GetStaticPropsResult,
+  NextPage,
+} from "next";
+
+import Page from "@components/layout/common/page";
+import { services } from "@site-data";
+import { ParsedUrlQuery } from "querystring";
+import PageCover from "@components/layout/common/page-cover";
+import CTASection from "@components/layout/common/cta-section";
+import FAQSection from "@components/layout/common/faq-section";
+import AboutSection from "@components/layout/service-page/about-section";
+import StepSection from "@components/layout/service-page/steps-section";
+import CallRequest from "@components/layout/common/call-request";
+
+interface Props {
+  service: Service;
+}
+
+const ServicePage: NextPage<Props> = ({ service }: Props): JSX.Element => {
+  return (
+    <Page title={service.name}>
+      <PageCover
+        cover={{
+          heading: service.name,
+          description: service.description,
+          id: service.id,
+          image: service.image,
+        }}
+      />
+      <AboutSection service={service} />
+      <StepSection steps={service.steps} />
+      <CTASection heading="Ready to get on-board with us?" />
+      <FAQSection faqs={service.faqs} />
+      <CallRequest />
+    </Page>
+  );
+};
+
+const getStaticProps: GetStaticProps = async (
+  context: GetStaticPropsContext
+): Promise<GetStaticPropsResult<Props>> => {
+  return {
+    props: {
+      service: services.find(
+        (service) => service.id === (context.params as ParsedUrlQuery).serviceId
+      ) as Service,
+    },
+  };
+};
+
+const getStaticPaths: GetStaticPaths = async (
+  context: GetStaticPathsContext
+): Promise<GetStaticPathsResult> => {
+  const paths: { params: { serviceId: string } }[] = services.map(
+    (service) => ({ params: { serviceId: service.id } })
+  );
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export { getStaticProps, getStaticPaths };
+
+export default ServicePage;
