dd164a5cb0b82858fd2e0f407eb03e27e5b30a62
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2021-08-31 17:31:38 +0530

    added remote image config to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   env: {
     MAP_BOX_API_TOKEN: process.env.MAP_BOX_API_TOKEN,
+    CONTENTFUL_DELIVERY_API_TOKEN: process.env.CONTENTFUL_DELIVERY_API_TOKEN,
+    CONTENTFUL_SPACE_ID: process.env.CONTENTFUL_SPACE_ID,
+  },
+  images: {
+    domains: ["images.ctfassets.net"],
   },
 };
