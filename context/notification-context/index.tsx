87a69fab6bea567c4cc5f307b057dae2c15215e9
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2021-08-31 22:28:38 +0530

    added notification context

---
+++ context/notification-context/index.tsx
@@ -0,0 +1,30 @@
+import { createContext, useState, ReactNode, Context } from "react";
+
+interface Props {
+  children: ReactNode;
+}
+
+const notificationContext: Context<string | null> = createContext<
+  string | null
+>("");
+const notificationDispatchContext: Context<DispatchFn<string | null> | null> =
+  createContext<DispatchFn<string | null> | null>(null);
+
+const NotificationContextProvider: React.FC<Props> = ({
+  children,
+}: Props): JSX.Element => {
+  const [notificationMessage, setNotificationMessage] = useState<string | null>(
+    ""
+  );
+
+  return (
+    <notificationContext.Provider value={notificationMessage}>
+      <notificationDispatchContext.Provider value={setNotificationMessage}>
+        {children}
+      </notificationDispatchContext.Provider>
+    </notificationContext.Provider>
+  );
+};
+
+export { notificationContext, notificationDispatchContext };
+export default NotificationContextProvider;
