eb113ff7f56acd539a0e890c9ec70c6121b84c8c
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2021-09-04 02:02:39 +0530

    added ADMIN_EMAIL & COURIER_ACCESS_TOKEN envs

--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@ module.exports = {
     MAP_BOX_API_TOKEN: process.env.MAP_BOX_API_TOKEN,
     CONTENTFUL_DELIVERY_API_TOKEN: process.env.CONTENTFUL_DELIVERY_API_TOKEN,
     CONTENTFUL_SPACE_ID: process.env.CONTENTFUL_SPACE_ID,
+    ADMIN_EMAIL: process.env.ADMIN_EMAIL,
+    COURIER_ACCESS_TOKEN: process.env.COURIER_ACCESS_TOKEN,
   },
   images: {
     domains: ["images.ctfassets.net"],
