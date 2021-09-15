0922850f9704ff9c61351610b572f307ac63bf36
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2021-09-15 11:32:05 +0530

    created tax-calculator route

---
+++ pages/tax-calculator.tsx
@@ -0,0 +1,12 @@
+import Page from "@components/layout/common/page";
+import { NextPage } from "next";
+
+const TaxCalculator: NextPage = (): JSX.Element => {
+  return (
+    <Page>
+      <div>Tax Calculator</div>
+    </Page>
+  );
+};
+
+export default TaxCalculator;
