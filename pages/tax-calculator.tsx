f3d0ba844763220d85a11819cdf21aa2665f04df
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2021-09-17 00:34:11 +0530

    added CalculatorParent component

--- pages/tax-calculator.tsx
+++ pages/tax-calculator.tsx
@@ -1,10 +1,11 @@
 import Page from "@components/layout/common/page";
+import CalculatorParent from "@components/layout/tax-calculator/calculator-parent";
 import { NextPage } from "next";
 
 const TaxCalculator: NextPage = (): JSX.Element => {
   return (
     <Page>
-      <div>Tax Calculator</div>
+      <CalculatorParent />
     </Page>
   );
 };
