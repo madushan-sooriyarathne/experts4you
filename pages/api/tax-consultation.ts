761dc578bf82d245cf831d1efa9c39790d1cf9ae
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2021-09-17 00:35:23 +0530

    added serverless function for tax calculator inquiry

---
+++ pages/api/tax-consultation.ts
@@ -0,0 +1,45 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import {
+  sendConsultationRequestEmail,
+  sendTaxConsultationRequestEmail,
+} from "utils/courier";
+
+export default async (
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseBody>
+): Promise<void> => {
+  // check the method
+  if (req.method !== "POST") {
+    res.status(405).json({
+      status: "failed",
+      message: `${req.method} method is not allowed in this route`,
+    });
+    return;
+  }
+
+  const inquiryData: TaxConsultationEmailType = { ...req.body };
+
+  // check request body fields
+  if (!inquiryData.email || !inquiryData.name || !inquiryData.phone) {
+    res.status(400).json({
+      status: "failed",
+      message: "Some required fields are missing in the HTTP request",
+    });
+    return;
+  }
+
+  const messageId = await sendTaxConsultationRequestEmail(inquiryData);
+
+  if (messageId) {
+    res.status(200).json({
+      status: "success",
+    });
+    return;
+  } else {
+    res.status(500).json({
+      status: "failed",
+      message: "Error occurred while submitting the the details",
+    });
+    return;
+  }
+};
